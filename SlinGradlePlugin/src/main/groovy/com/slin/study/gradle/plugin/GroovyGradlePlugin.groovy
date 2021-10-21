6d713c5c6fafd3073450fb5612c590467692329b
slin_huibo <dxslin@qq.com>
2021-10-21 11:23:29 +0800

    Initial commit

---
+++ SlinGradlePlugin/src/main/groovy/com/slin/study/gradle/plugin/GroovyGradlePlugin.groovy
@@ -0,0 +1,13 @@
+package com.slin.study.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GroovyGradlePlugin implements Plugin<Project>{
+
+
+    @Override
+    void apply(Project target) {
+
+    }
+}
---
+++ SlinGradlePlugin/src/main/java/com/slin/study/gradle/plugin/JavaGradlePlugin.java
@@ -0,0 +1,18 @@
+package com.slin.study.gradle.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * author: slin
+ * date: 2021/10/20
+ * description:
+ */
+public class JavaGradlePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+
+    }
+
+}
